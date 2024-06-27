799c2fa1da01896f5a09965a273f7c2823ecfbfb
Ali zaib <alizaib12220@gmail.com>
2024-06-27 19:50:33 +0500

    Associative array in php

---
+++ Arrays/associative-arr.php
@@ -0,0 +1,19 @@
+
+<?php
+    
+// Asscociative Array in php
+
+$age = [
+
+    "ali" => 25,
+    "noman" => 30,
+    "zainab" => 28
+];
+
+$age["ali"] = 20;
+
+echo "<pre>";
+
+var_dump($age);
+
+echo "</pre>";
