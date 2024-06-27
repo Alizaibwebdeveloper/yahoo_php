b5b196702440ff1a263236a76cdf8831f09927ff
Ali zaib <alizaib12220@gmail.com>
2024-06-27 18:34:54 +0500

    index array in php

---
+++ Arrays/index-arr.php
@@ -0,0 +1,15 @@
+<?php
+    
+// Index array in php
+
+ $colors = ["red", " yellow", " green", "blue", " orange"];
+
+ echo "<ul>";
+
+for($i =0; $i<=4; $i++){
+
+       echo "<li>" .$colors[$i]."<br>".  "</li>";
+
+}
+
+echo "</ul>";
