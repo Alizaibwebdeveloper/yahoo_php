3b39a7b37845658e37780ec0f6af6ae063a44e1f
Ali zaib <alizaib12220@gmail.com>
2024-06-27 14:22:29 +0500

    continue and break statement in php

---
+++ con-break.php
@@ -0,0 +1,18 @@
+
+<?php
+    
+
+// continue and break statement in php
+
+
+for($a = 1; $a<=10; $a++){
+
+
+    echo "Number : ". $a."<br>";
+
+    if( $a==5){
+        echo "no :";
+        continue;
+    }
+
+}
