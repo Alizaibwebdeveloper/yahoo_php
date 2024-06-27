e3eb00aa5959f398d424792012646ca8395600e7
Ali zaib <alizaib12220@gmail.com>
2024-06-27 14:35:32 +0500

    goto statement in php

---
+++ goto.php
@@ -0,0 +1,16 @@
+
+<?php
+    
+// goto statement in php
+
+echo " Hello  Ali zaib <br> ";
+echo " Hello  Ali zaib <br> ";
+goto abc;
+echo " Hello  Ali zaib <br> ";
+echo " Hello  Ali zaib <br> ";
+echo " Hello  Ali zaib <br> ";
+echo " Hello  Ali zaib <br> ";
+
+
+abc:
+echo " I am label abc ";
