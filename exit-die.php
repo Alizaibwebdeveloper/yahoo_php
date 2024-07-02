be71e2ffb83fba935a120e0bece33af75a8ca460
Ali zaib <alizaib12220@gmail.com>
2024-07-02 20:30:22 +0500

    exit and die functions

---
+++ exit-die.php
@@ -0,0 +1,11 @@
+
+<?php
+    
+
+    // exit() and die() function
+
+    echo "Hello World!<br>";
+    echo "Hello World!<br>";
+    die('Here is some error!!');
+    echo "Hello World!<br>";
+    echo "Hello World!<br>";
